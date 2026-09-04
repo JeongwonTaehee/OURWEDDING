abaaea8e9315091ea4c1426d2efdd408a22488fd
JeongwonTaehee <gusskth2331@gmail.com>
2026-09-04 19:23:56 +0900

    Update config.js

--- config.js
+++ config.js
@@ -56,20 +56,20 @@ const CONFIG = {
   story: {
     title: "PROFILE",
     groom: {
-      image : "groom.jpg",
+      image : "./image/groom.jpg",
       role : "신랑",
       name : "김태희",
       birth : "1998년 서울 장안동 출생",
       job : "김중사🫡",
-      description : "이거 맞아? ISTJ\n 자칭 낭만 로맨틱가이🌹\n 헬창이 되고싶은 헬짱💪🏻\n "
+      description : "이거 맞아? ISTJ \n 자칭 낭만 로맨틱가이🌹\n 헬창이 되고싶은 헬짱💪🏻 \n "
     },    
     bride : {
-      image : "bride.jpg",
+      image : "./image/bride.jpg",
       role : "신부",
       name : "문정원",
       birth : "1998년 성수동 출생",
       job : "문주무관",
-      description : "사람 좋아 ESFP\n 킹꼬물, 킹해물 누나🐶\n 최근 허남준에 빠짐\n "
+      description : "사람 좋아 ESFP \n 킹꼬물, 킹해물 누나🐶\n 최근 허남준에 빠짐 \n "
     }
   },
 
